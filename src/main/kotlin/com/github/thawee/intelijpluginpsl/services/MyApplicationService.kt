401c9eac51c52196719c9a9024c30d6e61b7a1f2
GitHub Action <action@github.com>
2022-11-03 03:52:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/thawee/intelijpluginpsl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/thawee/intelijpluginpsl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.thawee.intelijpluginpsl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thawee.intelijpluginpsl.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/thawee/intelijpluginpsl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thawee/intelijpluginpsl/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.thawee.intelijpluginpsl.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thawee.intelijpluginpsl.MyBundle
 
 class MyProjectService(project: Project) {
 
